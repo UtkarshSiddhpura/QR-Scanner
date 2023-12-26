f67f301801da25112f8bf34d13da3d05b23c8577
UtkarshSiddhpura <90092555+UtkarshSiddhpura@users.noreply.github.com>
2023-12-26 16:55:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function onScanSuccess(decodedText, decodedResult) {
+	console.log(`Scan result: ${decodedText}`, decodedResult);
+	html5QrcodeScanner.clear();
+
+	document.getElementById("refreshButton").style.display = "block";
+}
+
+function refreshScanner() {
+	location.reload(true);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+	fps: 10,
+	qrbox: 200,
+});
+
+html5QrcodeScanner.render(onScanSuccess);
